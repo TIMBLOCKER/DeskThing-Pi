67be1c8d1b7f827833a19f653dced75f121a3268
Tim Borkowy <71724235+TIMBLOCKER@users.noreply.github.com>
2024-12-20 12:42:35 +0100

    Update deskthing-pi-installer.sh

--- deskthing-pi-installer.sh
+++ deskthing-pi-installer.sh
@@ -20,44 +20,78 @@ echo "..........   =%@+   ......................................................
 echo "...........        .............................................................................................................."
 
 #Step 1 Update/Upgrade Pi to newest Version
+echo "................................................................................................................................."
+echo "Step 1: Update/Upgrade Pi to newest Version"
+echo "................................................................................................................................."
 apt update
 apt full-upgrade -y
 
+
 #Check and Install rpi-connect for remote access (can be skipped if connected with physical access)
-if which rpi-connect > /dev/null
+while true; do
+    read -p "Do you wish to install rpi-connect for remote access [Y/N]? " yn
+    case $yn in
+        [Yy]* ) 
+        if which rpi-connect > /dev/null
     then
-        echo "rpi-connect is installed - no installation needed"
+        echo "rpi-connect is already installed - No installation needed!"
     else
+    echo "................................................................................................................................."
+    echo "Installing rpi-connect and starting server"
+    echo "................................................................................................................................."
         apt install rpi-connect
+        rpi-connect on; 
+        rpi-connect signin
     fi
-rpi-connect on
+
+break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer Yy (Yes) or Nn (No).";;
+    esac
+done
+
+echo "................................................................................................................................."
+echo "Step 2: Check and install dependencies (node/npm/electron/electron-vite)"
+echo "................................................................................................................................."
 
 
 #Check and Install node as a base for the deskthing-server
 if which node > /dev/null
     then
-        echo "node is installed - no installation needed"
+        echo "node is installed - No installation needed"
     else
-        apt install nodejs -y
+    echo "................................................................................................................................."
+    echo "Installing node"
+    echo "................................................................................................................................."
+        apt install node -y
     fi
 
 #Check and Install npm as a package manager for downloading repo
 if which npm > /dev/null
     then
-        echo "node is installed - no installation needed"
+        echo "npm is installed - No installation needed"
     else
+    echo "................................................................................................................................."
+    echo "Installing npm"
+    echo "................................................................................................................................."
         apt install npm -y
     fi
 
 #Check and Install electron-vite for running deskthing-client locally
 if which electron-vite > /dev/null
     then
-        echo "electron-vite is installed - no installation needed"
+        echo "electron-vite is installed - No installation needed"
     else
+    echo "................................................................................................................................."
+    echo "Installing electron and electron-vite"
+    echo "................................................................................................................................."
         npm install electron-vite
         npm install electron
     fi    
 
+echo "................................................................................................................................."
+echo "Step 3: Clone ItsRiprod/DeskThing"
+echo "................................................................................................................................."
 
 #Git clone DeskThing
 git clone https://github.com/ItsRiprod/DeskThing DeskThing
@@ -68,7 +102,9 @@ chmod -R a+rwx ~/node_modules
 #Change into DeskThing Server Directory
 cd DeskThing/DeskThingServer
 ifconfig #get IP Adress
-sudo -u $USER npm run dev --host #DeskThing can be accessed from outside with IP-Adress and Port 8891
 
-#Display DeskThing in FullScreen on RPi
-DISPLAY=:0.0 chromium-browser --noerrdialogs --incognito --kiosk http://localhost:8891/
+echo "  ___      _                __ _      _    _           _ _ "
+echo " / __| ___| |_ _  _ _ __   / _(_)_ _ (_)__| |_  ___ __| | |"
+echo " \__ \/ -_)  _| || | '_ \ |  _| | ' \| (_-< ' \/ -_) _` |_|"
+echo " |___/\___|\__|\_,_| .__/ |_| |_|_||_|_/__/_||_\___\__,_(_)"
+echo "                   |_|                                     "
