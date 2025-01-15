7364991494c7ed9e6db9b23732366d0f0894593e
Tim Borkowy <71724235+TIMBLOCKER@users.noreply.github.com>
2025-01-15 13:45:09 +0100

    bash completion

--- deskthing-pi-installer.sh
+++ deskthing-pi-installer.sh
@@ -36,6 +36,10 @@ echo "..........................................................................
 #Check and Install nvm as a base for the deskthing-server
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install node
 
 nvm install-latest-npm
